1c236ab3e3d7c93e67f5d9ea30c97bddfca15691
Antony Leme <antony040402@gmail.com>
2022-12-05 22:39:16 -0300

    fix: result page header

--- src/routes/pages/Result/index.tsx
+++ src/routes/pages/Result/index.tsx
@@ -1,8 +1,7 @@
 import {
-  Box, Flex, Image, Text, useToast,
+  Box, Flex, useToast,
 } from '@chakra-ui/react';
 import React from 'react';
-import MozioLogo from 'assets/mozio.png';
 import FormResult from 'components/elements/FormResult';
 import { useNavigate, useSearchParams } from 'react-router-dom';
 import Header from 'components/elements/Header';
@@ -29,15 +28,6 @@ const ResultPage: React.FC = function () {
   return (
     <Flex h="100vh" justifyContent="center" py="128px">
       <Box w={{ base: '100%', lg: '380px' }} p={{ base: '24px', lg: 0 }} maxW="100%">
-        <Box textAlign="center">
-          <Image mx="auto" src={MozioLogo} data-testid="logo-image" />
-          <Text fontSize="14px" mb="24px" data-testid="welcome-message">
-            <strong>Well done!</strong>
-            {' '}
-            This is the result of your search
-          </Text>
-        </Box>
-
         <Header>
           <>
             <strong>Well done!</strong>
